7715d5d7156a0eeecf3bfbf0064ab8efefe8715a
ilearnonthisaccount <raynier_timothy_chua@dlsu.edu.ph>
2021-08-16 17:58:32 +0800

    Changes: - added layouts for Login, Sign Up, Add Restroom

---
+++ app/src/main/java/com/mobdeve/s15/g16/restroomlocator/AddRestroomActivity.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s15.g16.restroomlocator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddRestroomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_restroom);
+    }
+}
