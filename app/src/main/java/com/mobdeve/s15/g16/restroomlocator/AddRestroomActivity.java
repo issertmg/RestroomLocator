e637b43a977387237b547d94ab1c54029bf0443e
ilearnonthisaccount <raynier_timothy_chua@dlsu.edu.ph>
2021-08-18 12:09:17 +0800

    Implemented receiving image file from file selector

--- app/src/main/java/com/mobdeve/s15/g16/restroomlocator/AddRestroomActivity.java
+++ app/src/main/java/com/mobdeve/s15/g16/restroomlocator/AddRestroomActivity.java
@@ -1,25 +1,96 @@
 package com.mobdeve.s15.g16.restroomlocator;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.io.File;
 import java.util.Calendar;
 
 public class AddRestroomActivity extends AppCompatActivity {
+    private TextView tvStartTime, tvEndTime;
+    private ImageButton ibImgOne, ibImgTwo, ibImgThree;
+    private int padding_dp = 2;
+    private int padding_px;
+
+    private ActivityResultLauncher<Intent> galleryActivityResultLauncherOne = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        Uri imageUri = data.getData();
+                        String path = imageUri.getPath();
+                        File file = new File(path);
+
+                        ibImgOne.setImageURI(imageUri);
+                        ibImgOne.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                        ibImgOne.setPadding(padding_px, padding_px, padding_px, padding_px);
+                    }
+                }
+            });
+
+    private ActivityResultLauncher<Intent> galleryActivityResultLauncherTwo = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        Uri imageUri = data.getData();
+                        String path = imageUri.getPath();
+                        File file = new File(path);
+
+                        ibImgTwo.setImageURI(imageUri);
+                        ibImgTwo.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                        ibImgTwo.setPadding(padding_px, padding_px, padding_px, padding_px);
+                    }
+                }
+            });
+
+    private ActivityResultLauncher<Intent> galleryActivityResultLauncherThree = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        Uri imageUri = data.getData();
+                        String path = imageUri.getPath();
+                        File file = new File(path);
+
+                        ibImgThree.setImageURI(imageUri);
+                        ibImgThree.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                        ibImgThree.setPadding(padding_px, padding_px, padding_px, padding_px);
+                    }
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_restroom);
 
-        TextView tvStartTime = findViewById(R.id.tvOperatingStart);
-        TextView tvEndTime = findViewById(R.id.tvOperatingEnd);
+        padding_px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, padding_dp, AddRestroomActivity.this.getResources().getDisplayMetrics());
+
+        // setting TimePickerDialog to pop up on click
+        tvStartTime = findViewById(R.id.tvOperatingStart);
+        tvEndTime = findViewById(R.id.tvOperatingEnd);
 
         tvStartTime.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,6 +139,41 @@ public class AddRestroomActivity extends AppCompatActivity {
                 }, hour, minute, true);
                 timePicker.setTitle("Select Time");
                 timePicker.show();
+            }
+        });
+
+        ibImgOne = findViewById(R.id.ibImgOne);
+        ibImgTwo = findViewById(R.id.ibImgTwo);
+        ibImgThree = findViewById(R.id.ibImgThree);
+
+        // retrieving image from gallery
+        ibImgOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery_intent = new Intent();
+                gallery_intent.setAction(Intent.ACTION_GET_CONTENT);
+                gallery_intent.setType("image/*");
+                galleryActivityResultLauncherOne.launch(gallery_intent);
+            }
+        });
+
+        ibImgTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery_intent = new Intent();
+                gallery_intent.setAction(Intent.ACTION_GET_CONTENT);
+                gallery_intent.setType("image/*");
+                galleryActivityResultLauncherTwo.launch(gallery_intent);
+            }
+        });
+
+        ibImgThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery_intent = new Intent();
+                gallery_intent.setAction(Intent.ACTION_GET_CONTENT);
+                gallery_intent.setType("image/*");
+                galleryActivityResultLauncherThree.launch(gallery_intent);
             }
         });
     }
