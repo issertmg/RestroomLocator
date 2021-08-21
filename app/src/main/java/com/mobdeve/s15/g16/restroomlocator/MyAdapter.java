3e90bae0f9645f0942403a651d6b70b0e733771d
Mikaela Matias <maria_angela_mikaela_matias@dlsu.edu.ph>
2021-08-21 18:42:12 +0800

    Edited colors, completed recycler view layouts

---
+++ app/src/main/java/com/mobdeve/s15/g16/restroomlocator/MyAdapter.java
@@ -0,0 +1,8 @@
+package com.mobdeve.s15.g16.restroomlocator;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+    // TODO:
+}
