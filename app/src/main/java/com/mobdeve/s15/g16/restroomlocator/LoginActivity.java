ee449728907de2008d66a440443e79a79caa9808
issertmg <issertmg@gmail.com>
2021-08-24 01:19:01 +0800

    added sign up and login function (still bare)

--- app/src/main/java/com/mobdeve/s15/g16/restroomlocator/LoginActivity.java
+++ app/src/main/java/com/mobdeve/s15/g16/restroomlocator/LoginActivity.java
@@ -1,14 +1,91 @@
 package com.mobdeve.s15.g16.restroomlocator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etvUsername, etvPassword;
+    private Button btnLogin, btnSignUp, btnGuest;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etvUsername = findViewById(R.id.etvUsername);
+        etvPassword = findViewById(R.id.etvPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnGuest = findViewById(R.id.btnGuest);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnGuest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, ViewRestroomsNearbyActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void login() {
+        String email = etvUsername.getText().toString() + SignUpActivity.DOMAIN;
+        String password = etvPassword.getText().toString();
+
+        // TODO show animation while getting info from firebase
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success
+                            Toast.makeText(LoginActivity.this, "Login successful.",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(LoginActivity.this, ViewRestroomsNearbyActivity.class);
+                            startActivity(i);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("LOGIN", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/mobdeve/s15/g16/restroomlocator/SignUpActivity.java
+++ app/src/main/java/com/mobdeve/s15/g16/restroomlocator/SignUpActivity.java
@@ -1,14 +1,100 @@
 package com.mobdeve.s15.g16.restroomlocator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText etvUsername, etvPassword;
+    private Button btnSignUp;
+    private FirebaseAuth mAuth;
+
+    public static String DOMAIN = "@restroom.locator.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        etvUsername = findViewById(R.id.etvUsername);
+        etvPassword = findViewById(R.id.etvPassword);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
+
+    private void signUp() {
+        // TODO check username & password input constraints (length)
+        String username = etvUsername.getText().toString();
+        String password = etvPassword.getText().toString();
+
+        String email =  username + DOMAIN;
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign up successful
+                            // Create document for User (write to db)
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            MyFirestoreReferences.getUserCollectionReference()
+                                    .document(user.getUid())
+                                    .set(new User(username))
+                                    .addOnSuccessListener(new OnSuccessListener<Void>(){
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            FirebaseAuth.getInstance().signOut();
+                                            Toast.makeText(SignUpActivity.this, "Account successfully created.",
+                                                    Toast.LENGTH_SHORT).show();
+                                            finish();
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                        // TODO Show message if failed
+                                        }
+                                    });
+
+                        } else {
+                            // If sign up fails, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
 }
