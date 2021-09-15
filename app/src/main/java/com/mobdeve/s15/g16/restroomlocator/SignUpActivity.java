0c97df20ad44f1d1e56316608fac5165d5dccf90
issertmg <issertmg@gmail.com>
2021-09-16 00:38:49 +0800

    added confirm password field in Sign Up activity

--- app/src/main/java/com/mobdeve/s15/g16/restroomlocator/SignUpActivity.java
+++ app/src/main/java/com/mobdeve/s15/g16/restroomlocator/SignUpActivity.java
@@ -13,7 +13,7 @@ import com.mobdeve.s15.g16.restroomlocator.utils.MyFirestoreReferences;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    private EditText etvUsername, etvPassword;
+    private EditText etvUsername, etvPassword, etvConfirmPassword;
     private Button btnSignUp;
 
     @Override
@@ -23,6 +23,7 @@ public class SignUpActivity extends AppCompatActivity {
 
         etvUsername = findViewById(R.id.etvUsername);
         etvPassword = findViewById(R.id.etvPassword);
+        etvConfirmPassword = findViewById(R.id.etvConfirmPassword);
         btnSignUp = findViewById(R.id.btnSignUp);
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +38,7 @@ public class SignUpActivity extends AppCompatActivity {
     private void signUp() {
         String username = etvUsername.getText().toString();
         String password = etvPassword.getText().toString();
+        String confirmPassword = etvConfirmPassword.getText().toString();
 
         boolean isValidUsername = (username.length() >= 3 && username.length() <= 20);
         boolean isValidPassword = (password.length() >= 8);
@@ -47,6 +49,10 @@ public class SignUpActivity extends AppCompatActivity {
         }
         else if (!isValidPassword) {
             Toast.makeText(this, "Error: Password must be at least 8 characters in length.",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else if (!password.equals(confirmPassword)) {
+            Toast.makeText(this, "Error: Password and confirm password fields do not match.",
                     Toast.LENGTH_SHORT).show();
         }
         else {
